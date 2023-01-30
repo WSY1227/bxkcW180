9708b9c4302ed5845ba5021d5c2d001a3f00670a
1669228230@qq.com <2242933535@qq.com>
2023-01-30 11:53:50 +0800

    DS_24837初步测试完成

---
+++ src/main/java/com/bidizhaobiao/data/Crawl/service/impl/DS_24837/DS_24837_ZhaobGgService.java
@@ -0,0 +1,267 @@
+package com.bidizhaobiao.data.Crawl.service.impl.DS_24837;
+
+import com.bidizhaobiao.data.Crawl.entity.oracle.BranchNew;
+import com.bidizhaobiao.data.Crawl.entity.oracle.RecordVO;
+import com.bidizhaobiao.data.Crawl.service.MyDownloader;
+import com.bidizhaobiao.data.Crawl.service.SpiderService;
+import com.bidizhaobiao.data.Crawl.utils.CheckProclamationUtil;
+import com.bidizhaobiao.data.Crawl.utils.SpecialUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.HttpRequestBody;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.utils.HttpConstant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * 程序员：徐文帅 日期：2023-01-30
+ * 原网站：http://swj.yingtan.gov.cn/col/col1175/index.html
+ * 主页：http://swj.yingtan.gov.cn
+ **/
+@Service
+public class DS_24837_ZhaobGgService extends SpiderService implements PageProcessor {
+    public Spider spider = null;
+
+    public String listUrl = "http://swj.yingtan.gov.cn/module/web/jpage/dataproxy.jsp?startrecord=1&endrecord=39&perpage=13";
+    public String baseUrl = "http://swj.yingtan.gov.cn";
+    public Pattern datePat = Pattern.compile("(\\d{4})(年|/|-|\\.)(\\d{1,2})(月|/|-|\\.)(\\d{1,2})");
+
+    // 网站编号
+    public String sourceNum = "24837";
+    // 网站名称
+    public String sourceName = "鹰潭市商务局";
+    // 信息源
+    public String infoSource = "政府采购";
+    // 设置地区
+    public String area = "华东";
+    // 设置省份
+    public String province = "江西";
+    // 设置城市
+    public String city = "鹰潭";
+    // 设置县
+    public String district;
+    public String createBy = "徐文帅";
+    // 抓取网站的相关配置，包括：编码、抓取间隔、重试次数等
+    Site site = Site.me().setCycleRetryTimes(2).setTimeOut(30000).setSleepTime(20).setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.69");
+
+    public Site getSite() {
+        return this.site;
+    }
+
+    public Request getListRequest(int start, int end) {
+        Request request = new Request(listUrl.replace("startrecord=1", "startrecord=" + start).replace("endrecord=39", "endrecord=" + end));
+        request.setMethod(HttpConstant.Method.POST);
+        request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        //将键值对数组添加到map中
+        Map<String, Object> params = new HashMap<>();
+        params.put("col", "1");
+        params.put("webid", "24");
+        params.put("path", "http://swj.yingtan.gov.cn/");
+        params.put("columnid", "2931");
+        params.put("sourceContentType", "1");
+        params.put("unitid", "9827");
+        params.put("webname", "鹰潭市商务局");
+        params.put("permissiontype", "0");
+        //设置request参数
+        request.setRequestBody(HttpRequestBody.form(params, "utf-8"));
+        return request;
+    }
+
+    public void startCrawl(int ThreadNum, int crawlType) {
+        // 赋值
+        serviceContextEvaluation();
+        // 保存日志
+        serviceContext.setCrawlType(crawlType);
+        saveCrawlLog(serviceContext);
+        // 启动爬虫
+        spider = Spider.create(this).thread(ThreadNum)
+                .setDownloader(new MyDownloader(serviceContext, false, listUrl));
+        spider.addRequest(getListRequest(1, 39));
+        serviceContext.setSpider(spider);
+        spider.run();
+        // 爬虫状态监控部分
+        saveCrawlResult(serviceContext);
+    }
+
+    public void process(Page page) {
+        String url = page.getUrl().toString();
+        try {
+            List<BranchNew> detailList = new ArrayList<BranchNew>();
+            Thread.sleep(500);
+            if (url.contains("&perpage")) {
+                Document doc = Jsoup.parse(page.getRawText().replace("<![CDATA[", "").replace("]]>", ""));
+                Elements listElement = doc.select("record:has(a)");
+                if (listElement.size() > 0) {
+                    String key = "征集、询标、交易、机构、需求、废旧、废置、处置、报废、供应商、承销商、服务商、调研、优选、择选、择优、选取、公选、选定、摇选、摇号、摇珠、抽选、定选、定点、招标、采购、询价、询比、竞标、竞价、竞谈、竞拍、竞卖、竞买、竞投、竞租、比选、比价、竞争性、谈判、磋商、投标、邀标、议标、议价、单一来源、标段、明标、明投、出让、转让、拍卖、招租、出租、预审、发包、承包、分包、外包、开标、遴选、答疑、补遗、澄清、延期、挂牌、变更、预公告、监理、改造工程、报价、小额、零星、自采、商谈";
+                    String[] keys = key.split("、");
+                    for (Element element : listElement) {
+                        Element a = element.select("a").first();
+                        String link = a.attr("href").trim();
+                        String id = link.substring(link.lastIndexOf("/") + 1);
+                        String detailLink = link;
+                        String date = "";
+                        Matcher dateMat = datePat.matcher(element.text());
+                        if (dateMat.find()) {
+                            date = dateMat.group(1);
+                            date += dateMat.group(3).length() == 2 ? "-" + dateMat.group(3) : "-0" + dateMat.group(3);
+                            date += dateMat.group(5).length() == 2 ? "-" + dateMat.group(5) : "-0" + dateMat.group(5);
+                        }
+                        String title = a.attr("title").trim();
+                        if (title.length() < 2) title = a.text().trim();
+                        if (!CheckProclamationUtil.isProclamationValuable(title, keys)) {
+                            continue;
+                        }
+                        BranchNew branch = new BranchNew();
+                        branch.setId(id);
+                        serviceContext.setCurrentRecord(branch.getId());
+                        branch.setLink(link);
+                        branch.setDetailLink(detailLink);
+                        branch.setDate(date);
+                        branch.setTitle(title);
+                        detailList.add(branch);
+                    }
+                    List<BranchNew> branchNewList = checkData(detailList, serviceContext);
+                    for (BranchNew branch : branchNewList) {
+                        map.put(branch.getLink(), branch);
+                        page.addTargetRequest(new Request(branch.getLink()));
+                    }
+                } else {
+                    dealWithNullListPage(serviceContext);
+                }
+                if (serviceContext.getPageNum() == 1) {
+                    int total = Integer.parseInt(doc.select("totalrecord").text());
+                    serviceContext.setMaxPage(total % 39 == 0 ? total / 39 : total / 39 + 1);
+                }
+                if (serviceContext.getPageNum() < serviceContext.getMaxPage() && serviceContext.isNeedCrawl()) {
+                    serviceContext.setPageNum(serviceContext.getPageNum() + 1);
+                    int start = (serviceContext.getPageNum() - 1) * 39 + 1;
+                    page.addTargetRequest(getListRequest(start, start + 38));
+                }
+            } else {
+                BranchNew branch = map.get(url);
+                if (branch != null) {
+                    map.remove(url);
+                    serviceContext.setCurrentRecord(branch.getId());
+                    String detailHtml = page.getRawText();
+                    Document doc = Jsoup.parse(detailHtml);
+                    String title = branch.getTitle().replace("...", "");
+                    String date = branch.getDate();
+                    String content = "";
+                    Element contentElement = doc.select("div.my_cx_wzy").first();
+                    if (contentElement != null) {
+                        Elements aList = contentElement.select("a");
+                        for (Element a : aList) {
+                            String href = a.attr("href");
+                            a.attr("rel", "noreferrer");
+                            if (href.startsWith("mailto")) {
+                                continue;
+                            }
+                            if (href.contains("javascript") || href.equals("#")) {
+                                if (a.attr("onclick").contains("window.open('http")) {
+                                    String onclick = a.attr("onclick");
+                                    onclick = onclick.substring(onclick.indexOf("'") + 1, onclick.lastIndexOf("'"));
+                                    a.attr("href", onclick);
+                                    a.removeAttr("onclick");
+                                } else {
+                                    a.removeAttr("href");
+                                    a.removeAttr("onclick");
+                                }
+                                continue;
+                            }
+                            if (href.length() > 10 && href.indexOf("http") != 0) {
+                                if (href.indexOf("//www") == 0) {
+                                    href = baseUrl.substring(0, baseUrl.indexOf(":") + 1) + href;
+                                    a.attr("href", href);
+                                } else if (href.indexOf("../") == 0) {
+                                    href = href.replace("../", "");
+                                    href = baseUrl + "/" + href;
+                                    a.attr("href", href);
+                                } else if (href.startsWith("/")) {
+                                    href = baseUrl + href;
+                                    a.attr("href", href);
+                                } else if (href.indexOf("./") == 0) {
+                                    href = url.substring(0, url.lastIndexOf("/")) + href.substring(href.lastIndexOf("./") + 1);
+                                    a.attr("href", href);
+                                } else {
+                                    href = url.substring(0, url.lastIndexOf("/") + 1) + href;
+                                    a.attr("href", href);
+                                }
+                            }
+                        }
+                        Elements imgList = contentElement.select("IMG");
+                        for (Element img : imgList) {
+                            String src = img.attr("src");
+                            if (src.startsWith("file://")) {
+                                img.remove();
+                                continue;
+                            }
+                            if (src.contains("data:image")) {
+                                continue;
+                            }
+                            if (src.length() > 10 && src.indexOf("http") != 0) {
+                                if (src.indexOf("../") == 0) {
+                                    src = src.replace("../", "");
+                                    src = baseUrl + "/" + src;
+                                    img.attr("src", src);
+                                } else if (src.indexOf("./") == 0) {
+                                    src = url.substring(0, url.lastIndexOf("/")) + src.substring(src.lastIndexOf("./") + 1);
+                                    img.attr("src", src);
+                                } else if (src.startsWith("//")) {
+                                    src = baseUrl.substring(0, baseUrl.indexOf(":") + 1) + src;
+                                    img.attr("src", src);
+                                } else if (src.indexOf("/") == 0) {
+                                    src = baseUrl + src;
+                                    img.attr("src", src);
+                                } else {
+                                    src = url.substring(0, url.lastIndexOf("/") + 1) + src;
+                                    img.attr("src", src);
+                                }
+                            }
+                        }
+                        Element titleElement = contentElement.select("div.my_cx_wzy_top").first();
+                        if (titleElement != null) {
+                            titleElement.select("p").remove();
+                            title = titleElement.text().trim();
+                        }
+                        contentElement.select("div.my_cx_wzy_foot").remove();
+                        contentElement.select("script").remove();
+                        contentElement.select("style").remove();
+                        content = contentElement.outerHtml();
+                    } else if (url.contains(".doc") || url.contains(".pdf") || url.contains(".zip") || url.contains(".xls")) {
+                        content = "<div>附件下载：<a href='" + url + "'>" + branch.getTitle() + "</a></div>";
+                        detailHtml = Jsoup.parse(content).toString();
+                    }
+                    RecordVO recordVO = new RecordVO();
+                    recordVO.setId(branch.getId());
+                    recordVO.setListTitle(branch.getTitle());
+                    recordVO.setTitle(title);
+                    recordVO.setDetailLink(branch.getDetailLink());
+                    recordVO.setDetailHtml(detailHtml);
+                    recordVO.setDdid(SpecialUtil.stringMd5(detailHtml));
+                    recordVO.setDate(date);
+                    recordVO.setContent(content.replaceAll("\\ufeff|\\u2002|\\u200b|\\u2003", ""));
+                    dataStorage(serviceContext, recordVO, branch.getType());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            dealWithError(url, serviceContext, e);
+        }
+    }
+
+
+}
