f9762bb27f26fd28a77d01d5cb2ccec14a7e3204
1669228230@qq.com <2242933535@qq.com>
2023-02-03 17:02:29 +0800

    DX013406 bug修复

--- src/main/java/com/bidizhaobiao/data/Crawl/service/impl/DX013406/DX013406_ZhongbXxService.java
+++ src/main/java/com/bidizhaobiao/data/Crawl/service/impl/DX013406/DX013406_ZhongbXxService.java
@@ -1,4 +1,4 @@
-package com.bidizhaobiao.data.Crawl.service.impl.XX8426;
+package com.bidizhaobiao.data.Crawl.service.impl.DX013406;
 
 import com.bidizhaobiao.data.Crawl.entity.oracle.BranchNew;
 import com.bidizhaobiao.data.Crawl.entity.oracle.RecordVO;
@@ -24,35 +24,35 @@ import java.util.regex.Pattern;
 
 
 /**
- * 程序员：徐文帅 日期：2023-02-02
- * 原网站：http://www.ly1z.cn/NewsList.aspx
- * 主页：http://www.ly1z.cn
+ * 程序员：徐文帅 日期：2023-02-03
+ * 原网站：http://www.gdjiexin.com.cn/doc_24599230_0_0_1.html
+ * 主页：http://www.gdjiexin.com.cn
  **/
 @Service
-public class XX8426_1_ZhaobGgService extends SpiderService implements PageProcessor {
+public class DX013406_ZhongbXxService extends SpiderService implements PageProcessor {
     public Spider spider = null;
 
-    public String listUrl = "http://www.ly1z.cn/NewsList.aspx?page=1";
-    public String baseUrl = "http://www.ly1z.cn";
+    public String listUrl = "http://www.gdjiexin.com.cn/doc_24599230_0_0_1.html";
+    public String baseUrl = "http://www.gdjiexin.com.cn";
     public Pattern datePat = Pattern.compile("(\\d{4})(年|/|-|\\.)(\\d{1,2})(月|/|-|\\.)(\\d{1,2})");
 
     // 网站编号
-    public String sourceNum = "XX8426-1";
+    public String sourceNum = "DX013406";
     // 网站名称
-    public String sourceName = "洛阳市第四高级中学";
+    public String sourceName = "广东杰信项目管理有限公司";
     // 信息源
-    public String infoSource = "政府采购";
+    public String infoSource = "企业采购";
     // 设置地区
-    public String area = "华中";
+    public String area = "华南";
     // 设置省份
-    public String province = "河南";
+    public String province = "广东";
     // 设置城市
-    public String city = "洛阳";
+    public String city = "佛山";
     // 设置县
-    public String district = "瀍河";
+    public String district = "三水";
     public String createBy = "徐文帅";
     // 抓取网站的相关配置，包括：编码、抓取间隔、重试次数等
-    Site site = Site.me().setCycleRetryTimes(2).setTimeOut(30000).setSleepTime(20);
+    Site site = Site.me().setCycleRetryTimes(2).setTimeOut(30000).setSleepTime(20).setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.69");
 
     public Site getSite() {
         return this.site;
@@ -79,17 +79,15 @@ public class XX8426_1_ZhaobGgService extends SpiderService implements PageProces
         try {
             List<BranchNew> detailList = new ArrayList<BranchNew>();
             Thread.sleep(500);
-            if (url.contains("?page=")) {
+            if (url.contains("/doc_24599230_0_0_")) {
                 Document doc = Jsoup.parse(page.getRawText());
-                Elements listElement = doc.select(".list-b-r-m>ul>li:has(a)");
+                Elements listElement = doc.select("#text_listmodule_2>ol>li:has(a)");
                 if (listElement.size() > 0) {
-                    String key = "询标、交易、需求、废旧、废置、处置、报废、供应商、承销商、服务商、调研、择选、择优、选取、优选、公选、选定、摇选、摇号、摇珠、抽选、定选、定点、招标、采购、询价、询比、竞标、竞价、竞谈、竞拍、竞卖、竞买、竞投、竞租、比选、比价、竞争性、谈判、磋商、投标、邀标、议标、议价、单一来源、标段、明标、明投、出让、转让、拍卖、招租、出租、预审、发包、承包、分包、外包、开标、遴选、答疑、补遗、澄清、延期、挂牌、变更、预公告、监理、改造工程、报价、小额、零星、自采、商谈";
-                    String[] keys = key.split("、");
                     for (Element element : listElement) {
                         Element a = element.select("a").first();
                         String link = a.attr("href").trim();
                         String id = link.substring(link.lastIndexOf("?") + 1);
-                        link = baseUrl + "/" + link;
+                        link = url.substring(0, url.lastIndexOf("/") + 1) + link;
                         String detailLink = link;
                         String date = "";
                         Matcher dateMat = datePat.matcher(element.text());
@@ -100,7 +98,7 @@ public class XX8426_1_ZhaobGgService extends SpiderService implements PageProces
                         }
                         String title = a.attr("title").trim();
                         if (title.length() < 2) title = a.text().trim();
-                        if (!CheckProclamationUtil.isProclamationValuable(title, keys)) {
+                        if (!CheckProclamationUtil.isProclamationValuable(title)) {
                             continue;
                         }
                         BranchNew branch = new BranchNew();
@@ -120,10 +118,10 @@ public class XX8426_1_ZhaobGgService extends SpiderService implements PageProces
                 } else {
                     dealWithNullListPage(serviceContext);
                 }
-                Element nextPage = doc.select("#ContentPlaceHolder1_AspNetPager1 a:contains(下页)").first();
-                if (nextPage != null && nextPage.attr("href").contains("?page=") && serviceContext.isNeedCrawl()) {
-                    String href = baseUrl + "/" + nextPage.attr("href").trim();
+                Element nextPage = doc.select("a.page-next").first();
+                if (nextPage != null && nextPage.attr("href").contains("/24599230_0_0_") && serviceContext.isNeedCrawl()) {
                     serviceContext.setPageNum(serviceContext.getPageNum() + 1);
+                    String href = listUrl.replace("doc_24599230_0_0_1", "doc_24599230_0_0_" + serviceContext.getPageNum());
                     page.addTargetRequest(href);
                 }
             } else {
@@ -136,7 +134,7 @@ public class XX8426_1_ZhaobGgService extends SpiderService implements PageProces
                     String title = branch.getTitle().replace("...", "");
                     String date = branch.getDate();
                     String content = "";
-                    Element contentElement = doc.select("div.show-info").first();
+                    Element contentElement = doc.select("div#pShowListDetail").first();
                     if (contentElement != null) {
                         Elements aList = contentElement.select("a");
                         for (Element a : aList) {
@@ -207,7 +205,7 @@ public class XX8426_1_ZhaobGgService extends SpiderService implements PageProces
                                 }
                             }
                         }
-                        Element titleElement = doc.select("div.show-title").first();
+                        Element titleElement = doc.select("div#pDetailsTitle").first();
                         if (titleElement != null) {
                             title = titleElement.text().trim();
                         }
